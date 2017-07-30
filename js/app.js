5c6c27ec59302841af563ca5494c19d2da1b78fd
MarQwann Brown <marqwann@gmail.com>
2017-07-29 21:46:53 -0400

    adding JQuery project

---
+++ js/app.js
@@ -0,0 +1,22 @@
+//Problem: No user interaction causes no change to application
+//Solution: When user interacts causes changers appropriately
+var color = $("selected li").css("background-color");
+//When clickingon control list items
+$(".controls li").click(function () {
+	//Deselect sibling elements
+	//Select clicked elecment
+	//cache cu
+});
+
+//When new color is clicked
+	//Show color select or hide color select
+
+// When color sliders change
+	//update color preview span
+
+//when add color is pressed
+	//Append the color to the controls UI
+	//Select new color
+
+//On mouse events on the canvas
+	//Draw lines
